b4e2b38cc8fbf5f90d75faf3fc68bbc43030e333
Naveen M K <naveen521kk@gmail.com>
2024-11-18 17:21:36 +0530

    example: update to test with the library

--- example/App.tsx
+++ example/App.tsx
@@ -1,37 +1,87 @@
-import { useEvent } from 'expo';
-import ExpoHfTokenizers, { ExpoHfTokenizersView } from 'expo-hf-tokenizers';
-import { Button, SafeAreaView, ScrollView, Text, View } from 'react-native';
+import { useEvent } from "expo";
+import ExpoHfTokenizers from "expo-hf-tokenizers";
+import { Button, SafeAreaView, ScrollView, Text, View } from "react-native";
+import React from "react";
+import * as FileSystem from "expo-file-system";
+
+const downloadUrls = [
+  "https://github.com/chroma-core/onnx-embedding/raw/refs/heads/main/onnx/tokenizer.json",
+  "https://github.com/chroma-core/onnx-embedding/raw/refs/heads/main/onnx/config.json",
+  "https://github.com/chroma-core/onnx-embedding/raw/refs/heads/main/onnx/special_tokens_map.json",
+  "https://github.com/chroma-core/onnx-embedding/raw/refs/heads/main/onnx/tokenizer_config.json",
+  "https://github.com/chroma-core/onnx-embedding/raw/refs/heads/main/onnx/vocab.txt",
+];
 
 export default function App() {
-  const onChangePayload = useEvent(ExpoHfTokenizers, 'onChange');
+  const onChangePayload = useEvent(ExpoHfTokenizers, "onChange");
+  const [encoded, setEncoded] = React.useState("");
+  const [decoded, setDecoded] = React.useState("");
+
+  React.useEffect(() => {
+    const a = async () => {
+      console.log("oh well");
+
+      console.log("Downloading resources");
+      const tokenizerFilePath = FileSystem.cacheDirectory + "/tokenizer";
+
+      const exists = await FileSystem.getInfoAsync(tokenizerFilePath);
+      if (!exists.exists) {
+        await FileSystem.makeDirectoryAsync(tokenizerFilePath, {
+          intermediates: true,
+        });
+      }
+
+      // Download files
+      const downloadPromises = [];
+
+      for (const url of downloadUrls) {
+        const toFile = tokenizerFilePath + "/" + url.split("/").pop();
+
+        // Check if file exists
+        const fileExists = await FileSystem.getInfoAsync(toFile);
+        if (fileExists.exists) {
+          console.log("File already exists, skipping download: " + toFile);
+          continue;
+        }
+        console.log(`Downloading ${url} to ${toFile}`);
+        downloadPromises.push(
+          FileSystem.downloadAsync(url, toFile).then((res) => {
+            console.log("Downloaded " + toFile);
+          })
+        );
+      }
+
+      await Promise.all(downloadPromises);
+
+      // list the files
+      const files = await FileSystem.readDirectoryAsync(tokenizerFilePath);
+      console.log(files);
+
+      try {
+        const b = await ExpoHfTokenizers.encode(
+          tokenizerFilePath,
+          "Hello World, my name is Naveen M K."
+        );
+        console.log(b);
+        setEncoded(JSON.stringify(b, null, 4));
+
+        const c = await ExpoHfTokenizers.decode(tokenizerFilePath, b.ids);
+        console.log(c);
+        setDecoded(c);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    a();
+  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.container}>
         <Text style={styles.header}>Module API Example</Text>
-        <Group name="Constants">
-          <Text>{ExpoHfTokenizers.PI}</Text>
-        </Group>
         <Group name="Functions">
-          <Text>{ExpoHfTokenizers.hello()}</Text>
-        </Group>
-        <Group name="Async functions">
-          <Button
-            title="Set value"
-            onPress={async () => {
-              await ExpoHfTokenizers.setValueAsync('Hello from JS!');
-            }}
-          />
-        </Group>
-        <Group name="Events">
-          <Text>{onChangePayload?.value}</Text>
-        </Group>
-        <Group name="Views">
-          <ExpoHfTokenizersView
-            url="https://www.example.com"
-            onLoad={({ nativeEvent: { url } }) => console.log(`Loaded: ${url}`)}
-            style={styles.view}
-          />
+          <Text>{encoded}</Text>
+          <Text>{decoded}</Text>
         </Group>
       </ScrollView>
     </SafeAreaView>
@@ -58,13 +108,13 @@ const styles = {
   },
   group: {
     margin: 20,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderRadius: 10,
     padding: 20,
   },
   container: {
     flex: 1,
-    backgroundColor: '#eee',
+    backgroundColor: "#eee",
   },
   view: {
     flex: 1,
